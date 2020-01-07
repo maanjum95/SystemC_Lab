9760046adf558a04268bd52852e8c9f953db995a
M. Anjum <ma.anjum@tum.de>
2020-01-07 20:18:15 +0100

    ex_5 finalized. 19 out of 20 sent. 1 unkown.

--- ex_5/Cpu.cpp
+++ ex_5/Cpu.cpp
@@ -5,8 +5,13 @@
 using namespace sc_core;
 using namespace tlm;
 
+///  filename for reporting
+static const char *filename = "DmaChannel.cpp";
+
 void Cpu::processor_thread(void) {
 	tlm_response_status tlm_stat;
+	soc_address_t port_arr[] = {OUTPUT_0_ADDRESS, OUTPUT_1_ADDRESS, OUTPUT_2_ADDRESS, OUTPUT_3_ADDRESS};
+	int port_id;
 
 	while(true) {
 
@@ -15,59 +20,58 @@ void Cpu::processor_thread(void) {
 		//******************************************************
 
 		// check if packets are available
-		if (packetReceived_interrupt != true)
-			wait(packetReceived_interrupt.value_changed_event());
+		while (this->packetReceived_interrupt != true)
+			wait(this->packetReceived_interrupt.value_changed_event());
 
 		// starting transaction to get packet descriptor
-		startTransaction(TLM_READ_COMMAND, PROCESSOR_QUEUE_ADDRESS, (unsigned char *) &m_packet_descriptor, sizeof(m_packet_descriptor));
+		this->startTransaction(TLM_READ_COMMAND, PROCESSOR_QUEUE_ADDRESS, (unsigned char *) &this->m_packet_descriptor, sizeof(this->m_packet_descriptor));
 
-		// wait for appropriate response to be recieved
+		// wait for the transaction to be finished
 		wait(transactionFinished_event); 
+		
+		// get response status
+		tlm_stat = this->payload.get_response_status();
 
-		if (do_logging) {
-			cout << endl << sc_time_stamp()
-				<< "CPU::processor_thread : Transaction is finished";
+		// check status of response. 
+		// restart the loop if error is recieved.
+		if (tlm_stat == TLM_OK_RESPONSE) {
+			REPORT_ERROR(filename, __FUNCTION__, "Transaction finished successfully.");
+		} else {
+			REPORT_ERROR(filename, __FUNCTION__, "Transaction failed. Restarting loop...");
 			
-			tlm_stat = payload.get_response_status();
-
-			if (tlm_stat == TLM_OK_RESPONSE)
-				cout << " successfully." << endl;
-			else
-				cout << " unsuccessfully." << endl;
-			
-			cout << "CPU::processor_thread : Packet descriptor recieved: " << m_packet_descriptor << endl;
+			// since the transaction failed. restarting the function loop
+			continue;
 		}
+		
+		// logging the recieved packet descriptor
+		REPORT_INFO(filename, __FUNCTION__, "Packet descriptor recieved" << this->m_packet_descriptor );
 
 		//*********************************************************
 		// Forward the packet descriptor to an arbitrary port
 		//*********************************************************
 
 		// selecting a random port to send packet descriptor
-		soc_address_t port_arr[] = {OUTPUT_0_ADDRESS, OUTPUT_1_ADDRESS, OUTPUT_2_ADDRESS, OUTPUT_3_ADDRESS};
-		int port_id = rand() % 4;
-		if (do_logging) {
-			cout << "CPU::processor_thread : Sending to output port: " << port_id << endl;
-		}
+		port_id = rand() % 4;
+		
+		REPORT_INFO(filename, __FUNCTION__, "Send to output port " << port_id);
 
 		// starting transaction
-		startTransaction(TLM_WRITE_COMMAND, port_arr[port_id], (unsigned char *) &m_packet_descriptor, sizeof(m_packet_descriptor));
+		startTransaction(TLM_WRITE_COMMAND, port_arr[port_id], (unsigned char *) &this->m_packet_descriptor, sizeof(this->m_packet_descriptor));
 
 		// wait for appropriate response to be recieved
-		wait(transactionFinished_event); 
+		wait(this->transactionFinished_event); 
 
-		if (do_logging) {
-			cout << endl << sc_time_stamp()
-				<< "CPU::processor_thread : Transaction is finished";
-			
-			tlm_stat = payload.get_response_status();
-			
-			if (tlm_stat == TLM_OK_RESPONSE)
-				cout << " successfully." << endl;
-			else
-				cout << " unsuccessfully." << endl;
-			
-			cout << endl << "CPU::processor_thread : Wrote to output port:" << port_id << ", the value: " << m_packet_descriptor << endl;
-		}	
+		// get response status
+		tlm_stat = this->payload.get_response_status();
+		
+		if (tlm_stat == TLM_OK_RESPONSE) {
+			REPORT_INFO(filename, __FUNCTION__, "Transaction finished successfully.");
+		} else {
+			REPORT_INFO(filename, __FUNCTION__, "Transaction failed.");
+			cout << "unsuccessfull" << endl;
+		}
+		
+		REPORT_INFO(filename, __FUNCTION__, "Wrote to output port: " << port_id << ", the value: " << this->m_packet_descriptor);
 	}
 }
 
@@ -92,22 +96,13 @@ tlm_sync_enum Cpu::nb_transport_bw(tlm_generic_payload& transaction,
 	delay_time += CLK_CYCLE_BUS;
 
 	// respond to transaction after one CYCLE of the BUS.
-	transactionFinished_event.notify(delay_time);
+	this->transactionFinished_event.notify(delay_time);
 
 	// finish the transaction (end of 2nd phase)
 	phase = END_RESP;
 	return TLM_COMPLETED;
 }
 
-/**
-* Start a 2-phase transaction with the given arguments.
-*
-* @param command - TLM_READ_COMMAND or TLM_WRITE_COMMAND
-* @param address - the address of the destination/source of the data
-* @param data    - pointer to the data that is written or pointer to a
-*                  buffer where the data is going to be stored
-* @param dataSize - size of the data in bytes
-*/
 void Cpu::startTransaction(tlm_command command, soc_address_t address,
 	unsigned char *data, unsigned int dataSize)
 {
@@ -116,18 +111,18 @@ void Cpu::startTransaction(tlm_command command, soc_address_t address,
 	tlm_sync_enum tlm_resp;
 
 	// setting up the transaction payload
-	payload.set_command(command);
-	payload.set_data_ptr(data);
-	payload.set_address(address);
-	payload.set_data_length(dataSize);
-	payload.set_response_status(TLM_GENERIC_ERROR_RESPONSE);
+	this->payload.set_command(command);
+	this->payload.set_data_ptr(data);
+	this->payload.set_address(address);
+	this->payload.set_data_length(dataSize);
+	this->payload.set_response_status(TLM_GENERIC_ERROR_RESPONSE);
 
 	// prepare the transaction
 	delay_time = SC_ZERO_TIME;
 	phase = BEGIN_REQ;
 
 	// calling the transaction
-	tlm_resp = initiator_socket->nb_transport_fw(payload, phase, delay_time);
+	tlm_resp = this->initiator_socket->nb_transport_fw(payload, phase, delay_time);
 
 	if (do_logging) {
 		if (tlm_resp != TLM_UPDATED || phase != END_REQ) {
